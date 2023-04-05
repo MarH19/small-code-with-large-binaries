ca925416c97139dd736e86be794e2eace871a4f4
Christopher <tibaldo.chris@gmail.com>
2023-04-05 18:51:02 +0200

    Experimented with different ways of quantifying generated code size and restrict exessive reductions. Added binary size helper function

---
+++ project/helper.py
@@ -0,0 +1,16 @@
+import re
+
+
+# Source Stack-overflow: https://stackoverflow.com/questions/241327/remove-c-and-c-comments-using-python
+def comment_remover(text):
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith('/'):
+            return " " # note: a space and not an empty string
+        else:
+            return s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
--- project/initial_reducer.py
+++ project/initial_reducer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 """
-In this example diopter is used to generate and reduce a csmith
-that results in larger text with -Os than -O3
+This diopter program is used to generate and reduce a csmith
+that results in a disproportionate ratio between binary and source code
 """
 
+import tempfile
+import helper
 import subprocess
 from diopter.compiler import (
     CompilationSetting,
@@ -20,11 +22,12 @@ from diopter.compiler import Language
 
 import sys
 from subprocess import call
+import re
 
 
 # This is used to devalue the trivial solution
 def get_empty_assembly_size(setting: CompilationSetting) -> int:
-    empty = "int main(){}"
+    empty = '#include "csmith.h"\n int main(){}'
     program = SourceProgram(
             code=empty,
             language=Language.C,
@@ -36,7 +39,8 @@ def get_empty_assembly_size(setting: CompilationSetting) -> int:
 
     bianryLength = setting.compile_program(
         program, 
-        ObjectCompilationOutput(None)).output.text_size()
+        ObjectCompilationOutput(None),
+        additional_flags=["-I" "/home/chris/.bin/csmith/build/include/"]).output.text_size()
 
     return bianryLength
 
@@ -53,7 +57,6 @@ def get_ratio(program: SourceProgram, setting: CompilationSetting) -> float:
         program, ObjectCompilationOutput(None)
         ).output.text_size()
     
-    #TODO fixed ratio -> binaryLength-(default initializer for stack). Devalues `int main(){}` outputs
     ratio = (binaryLength-get_empty_assembly_size(setting))/codeLength
 
     return ratio
@@ -61,15 +64,43 @@ def get_ratio(program: SourceProgram, setting: CompilationSetting) -> float:
 
 def ratio_filter(program: SourceProgram, Osettings: CompilationSetting, best_ratio: float) -> bool:
     ratio = get_ratio(program,Osettings)
-    #1. clang-format code
-    lc = ["echo",program.code,"|", "clang-format",
-          '--style="{ BasedOnStyle: Google, KeepEmptyLinesAtTheStartOfBlocks: false, KeepEmptyLinesAtTheEndOfBlocks: false }"']
 
-    child = subprocess.Popen(lc, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output = child.stdout.read()
-    output = [s for s in output.splitlines() if not s.isspace()]    # Remove lines containing only spaces
-    
-    return ratio>best_ratio and len(output) > 15
+    #0. remove comments from code
+    code = helper.comment_remover(program.code)
+
+    #1. clang-format code
+    # lc = ["echo",code,"| clang-format"]
+    # #      ,'--style="{ BasedOnStyle: Google, KeepEmptyLinesAtTheStartOfBlocks: false, KeepEmptyLinesAtTheEndOfBlocks: false }"']
+    # child = subprocess.Popen(lc, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    # output = child.stdout.read()
+    proc = subprocess.Popen(
+        ["clang-format"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+    )
+    stdout, stderr = proc.communicate(input=code)
+    output = stdout
+
+    #OPTION 1, try to use a tool to count the logical lines of code/tokens in c code TODO find such a tools
+    with tempfile.NamedTemporaryFile(mode="w", suffix=".c", delete=True) as f:
+        f.write(code)
+
+        #cmd_str = "ctags -x "+f.name+" | awk '{print $2}' | sort | uniq | wc -l"
+        cmd_str = "ctags -x "+f.name+" | awk '{print $2}' | sort | wc -l"
+        
+        out = subprocess.run(cmd_str, shell=True, stdout=subprocess.PIPE)
+        
+        print(int(bytes.decode(out.stdout))>10) #ensure that there are more than 10 variable definitions/functions
+        
+
+    # OPTION 2, try to remove tricks by hand
+    #2. Remove "padding lines" which only contain ; { } ( )
+    output = [s for s in output.splitlines() if not (s.replace(";","")).isspace()] # Remove lines containing only spaces or only ;
+
+
+    return ratio>best_ratio and int(bytes.decode(out.stdout))>10 #and len(output) > 15
 
 
 class ReduceObjectSize(ReductionCallback):
@@ -95,27 +126,29 @@ class ReduceObjectSize(ReductionCallback):
         return False
 
 if __name__ == "__main__":
-    O3 = CompilationSetting(
-        compiler=CompilerExe.get_system_gcc(),
-        opt_level=OptLevel.O3,
-        flags=("-march=native",),
-    )
-    Os = CompilationSetting(
-        compiler=CompilerExe.get_system_gcc(),
-        opt_level=OptLevel.Os,
-        flags=("-march=native",),
-    )
-    sanitizer = Sanitizer()
-    while True:
-        p = CSmithGenerator(sanitizer,include_path="/home/chris/.bin/csmith/build/include").generate_program()
-        #p = Os.preprocess_program(p, make_compiler_agnostic=False)
+    for i in range(5):
+        O3 = CompilationSetting(
+            compiler=CompilerExe.get_system_gcc(),
+            opt_level=OptLevel.O3,
+            flags=("-march=native",),
+        )
+        Os = CompilationSetting(
+            compiler=CompilerExe.get_system_gcc(),
+            opt_level=OptLevel.Os,
+            flags=("-march=native",),
+        )
+        sanitizer = Sanitizer()
+        while True:
+            p = CSmithGenerator(sanitizer,include_path="/home/chris/.bin/csmith/build/include").generate_program()
+            #p = Os.preprocess_program(p, make_compiler_agnostic=False)
 
-        #print(ratio_filter(p,O3,0))
-        if ratio_filter(p, O3, 0):
-            break
-    print(f"initial ratio: {get_ratio(p, O3)}")
-    #print(p.code)    
-    rprogram = Reducer().reduce(p, ReduceObjectSize(sanitizer, O3, Os))  # , debug=True)
-    assert rprogram
-
-    print(f"Ratio obtained: {get_ratio(rprogram, O3)}")
+            #print(ratio_filter(p,O3,0))
+            if ratio_filter(p, O3, 0):
+                break
+        print(f"initial ratio: {get_ratio(p, O3)}")
+        #print(p.code)    
+        rprogram = Reducer().reduce(p, ReduceObjectSize(sanitizer, O3, Os))  # , debug=True)
+        assert rprogram
+        with open("output_"+str(i)+".c", "a") as f:
+            print(rprogram.code, file = f)
+        print(f"Ratio obtained: {get_ratio(rprogram, O3)}")
