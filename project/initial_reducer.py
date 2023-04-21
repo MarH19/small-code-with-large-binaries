d7fa72467109b6c5c2f6dfa535e45576d66f8a83
Christopher <tibaldo.chris@gmail.com>
2023-04-21 20:40:44 +0200

    Removed O3, since we focus on Os

--- project/initial_reducer.py
+++ project/initial_reducer.py
@@ -81,38 +81,22 @@ def ratio_filter(program: SourceProgram, Osettings: CompilationSetting, best_rat
     # )
     # stdout, stderr = proc.communicate(input=code)
     # output = stdout
+
     print("---")
     ast_code_size = ast_parser.get_code_size(code)
     print(ast_code_size)
     print("---")
-    #OPTION 1, try to use a tool to count the logical lines of code/tokens in c code TODO find such a tools
-    # with tempfile.NamedTemporaryFile(mode="w", suffix=".c", delete=True) as f:
-    #     f.write(code)
 
-    #     cmd_str = "ctags -x "+f.name+" | awk '{print $2}' | sort | wc -l"
-        
-    #     out = subprocess.run(cmd_str, shell=True, stdout=subprocess.PIPE)
-        
-    #     print(int(bytes.decode(out.stdout))>10) #ensure that there are more than 10 variable definitions/functions
-        
-
-    # OPTION 2, try to remove tricks by hand
-    #2. Remove "padding lines" which only contain ; { } ( )
-    # output = [s for s in output.splitlines() if not (s.replace(";","")).isspace()] # Remove lines containing only spaces or only ;
-
-
-    return ratio>best_ratio and ast_code_size >30 #int(bytes.decode(out.stdout))>10 #and len(output) > 15
+    return ratio>best_ratio and ast_code_size >30 
 
 
 class ReduceObjectSize(ReductionCallback):
     def __init__(
         self,
         san: Sanitizer,
-        O3: CompilationSetting,
         Os: CompilationSetting,
     ):
         self.san = san
-        self.O3 = O3
         self.Os = Os
         self.bestRatio = 0
 
@@ -122,18 +106,13 @@ class ReduceObjectSize(ReductionCallback):
             return False
         
         # Check if the ratio has increased!
-        if ratio_filter(program, self.O3, self.bestRatio):
-            self.bestRatio = get_ratio(program, self.O3)
+        if ratio_filter(program, self.Os, self.bestRatio):
+            self.bestRatio = get_ratio(program, self.Os)
             return True
         return False
 
 if __name__ == "__main__":
     for i in range(1):  #TODO define a better way to run multiple intresting
-        O3 = CompilationSetting(
-            compiler=CompilerExe.get_system_gcc(),
-            opt_level=OptLevel.O3,
-            flags=("-march=native",),
-        )
         Os = CompilationSetting(
             compiler=CompilerExe.get_system_gcc(),
             opt_level=OptLevel.Os,
@@ -149,15 +128,15 @@ if __name__ == "__main__":
             p = CSmithGenerator(sanitizer,include_path="/home/chris/.bin/csmith/build/include").generate_program()
             p = sanOs.preprocess_program(p, make_compiler_agnostic=True)
             sanitizer.sanitize(p)
-            if ratio_filter(p, O3, 0):
+            if ratio_filter(p, Os, 0):
                 break
-        print(f"initial ratio: {get_ratio(p, O3)}")
-        t = ReduceObjectSize(sanitizer, O3, Os)
+        print(f"initial ratio: {get_ratio(p, Os)}")
+        t = ReduceObjectSize(sanitizer, Os)
         # import pdb; pdb.set_trace()
-        rprogram = Reducer().reduce(p, ReduceObjectSize(sanitizer, O3, Os))  # , debug=True)
+        rprogram = Reducer().reduce(p, ReduceObjectSize(sanitizer, Os))  # , debug=True)
         assert rprogram
 
         #TODO ensure that output is clang-formatted for better readability
         with open("output_"+str(i)+".c", "a") as f:
             print(rprogram.code, file = f)
-        print(f"Ratio obtained: {get_ratio(rprogram, O3)}")
+        print(f"Ratio obtained: {get_ratio(rprogram, Os)}")
