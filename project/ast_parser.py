97476a4a751d5012dd6482d9db90ecc619870497
Christopher <tibaldo.chris@gmail.com>
2023-04-27 11:27:19 +0200

    Fixed AST parser for nth time

--- project/ast_parser.py
+++ project/ast_parser.py
@@ -1,5 +1,5 @@
 # Utilities to help parse Abstract syntax trees
-
+from argparse import ArgumentParser
 import subprocess
 import tempfile
 from anytree import Node, RenderTree
@@ -11,34 +11,23 @@ def count_ast_nodes():
 def parse_tree(parent: Node, previous_depth:int):
     global remaining_input
 
-    if len(remaining_input)==0:
-        return
     splitInnput = remaining_input[0].split("-")
     currentDepth = len(splitInnput[0])
     currentNode = Node(splitInnput[1],parent=parent)
     remaining_input = remaining_input[1:]
-    
-    while len(remaining_input)>0:
-        splitInnput = remaining_input[0].split("-")
-        node_depth = len(splitInnput[0])
-        if node_depth == currentDepth:
-            parse_tree(parent,currentDepth)
-            return
-        elif node_depth > currentDepth:
-            parse_tree(currentNode,currentDepth)
-        else:
-            return
-        remaining_input = remaining_input[1:]
-        
 
+    #print(splitInnput[1])
 
-    # We are at current node.
-    # If following node 1 deeper:
-    # Eat. Continue
-    # If following  node >1 deeper parse from observed node
-    # If following node empy: return
-    # if following node not deeper return
-    # if following node same depth as own: parse_tree
+    while len(remaining_input)!=0:
+        next_depth = len(remaining_input[0].split("-")[0])
+        if next_depth < currentDepth:
+            return
+        if next_depth == currentDepth:
+            currentDepth = next_depth
+            currentNode = Node(remaining_input[0].split("-")[1], parent=parent)
+            remaining_input = remaining_input[1:]
+        if next_depth > currentDepth:
+            parse_tree(currentNode, 0)
 
 def extract_ast_from_output(input):
     global remaining_input
@@ -84,4 +73,23 @@ def get_code_size(code: str) -> int:
     # 2. pass arguments to get_ast_size
         return get_ast_size(out.stdout.decode("utf-8"))
 
-#if called as main expect file as input and return ast of that file
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename",
+                        help="FILE from which to read", metavar="FILE")
+
+    args = parser.parse_args()
+    code = ""
+    cmd_str = "clang -Xclang -ast-dump -fsyntax-only "+args.filename
+    out = subprocess.run(cmd_str, shell=True, stdout=subprocess.PIPE)
+    get_ast_size(out.stdout.decode("utf-8"))
+    
+
+
+    # with open(args.filename, "r") as f:
+    #     for line in f.readlines():
+    #         code+=line
+
+    # ast_size = get_code_size(code)
+
+    # print(ast_size)
